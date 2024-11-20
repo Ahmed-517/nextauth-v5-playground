16e19d2d33527a17eb948c29cc9e60f8dae97925
Ahmed-517 <ahmed195041@feng.bu.edu.eg>
2024-11-20 09:23:41 +0200

    commit

---
+++ src/actions/register.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import * as z from "zod";
+import bcrypt from "bcrypt";
+
+import { db } from "@/lib/db";
+import { RegisterSchema } from "@/schemas";
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validateFields = RegisterSchema.safeParse(values);
+
+  if (!validateFields.success) {
+    return {
+      error: "Invalid field!",
+    };
+  }
+
+  const { email, password, name } = validateFields.data;
+
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  const existingUser = await db.user.findUnique({
+    where: { email },
+  });
+
+  if (existingUser) return { error: "Email already in use!" };
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send Verification token email
+
+  return { success: "Email sent!" };
+};
